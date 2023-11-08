7113ead36dafa870ae05e09d6022e53b1cc27454
Rebecca <moonbladez@hotmail.com>
2023-11-08 10:30:12 +0100

    displayed messages on the screen from firebase

--- lib/widgets/chat_message_list.dart
+++ lib/widgets/chat_message_list.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessageList extends StatelessWidget {
@@ -5,8 +6,38 @@ class ChatMessageList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No Message found'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection("chat")
+          .orderBy("created_at", descending: false)
+          .snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.hasError) {
+          return const Center(
+            child: Text("Something went wrong"),
+          );
+        }
+
+        final loadedMessages = snapshot.data!.docs;
+
+        if (!snapshot.hasData || loadedMessages.isEmpty) {
+          return const Center(
+            child: Text("No messages found"),
+          );
+        }
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) {
+            return Text(loadedMessages[index]["message"]);
+          },
+        );
+      },
     );
   }
 }
